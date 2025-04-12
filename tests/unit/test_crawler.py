74813e443c4454c3d598af06a2e581e9a440067f
James Hudson <james.hudson@bridge-u.com>
2025-04-13 00:23:03 +0100

    test: :white_check_mark: Update crawler test to expect usage of the MockParser class

--- tests/unit/test_crawler.py
+++ tests/unit/test_crawler.py
@@ -1,9 +1,25 @@
+import pytest
+
 from crawlspace import Crawler
 from crawlspace.schemas import PageReport, SiteReport
 
 
+class MockParser:
+    @staticmethod
+    def parse(url: str, page: str) -> list[str]:
+        match url:
+            case "http://localhost:8888":
+                return ["http://localhost:8888/empty-page"]
+
+            case "http://localhost:8888/empty-page":
+                return list()
+
+            case _:
+                return list()
+
+
 async def test_crawler():
-    crawler = Crawler()
+    crawler = Crawler(parser=MockParser)
     base_url = "http://localhost:8888"
 
     result = await crawler.crawl(base_url)
