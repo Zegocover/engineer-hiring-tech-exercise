5bb1cfa7cd5519b4a601806ee750c2f94eb69361
Alexander Wilkes <alexanderwilkes.personal@gmail.com>
2025-11-19 08:29:43 +0000

    Abstract class

---
+++ python/web_crawler/web_crawler/webcrawler.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+from web_crawler.url import URL
+
+
+class WebCrawler(ABC):
+    """Abstract class for web crawler. This is trivial but done for completeness."""
+    @abstractmethod
+    def crawl(self, url: URL | str) -> None:
+        pass
