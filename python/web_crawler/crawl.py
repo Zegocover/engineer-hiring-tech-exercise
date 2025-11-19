3157571441687d2a364d85bc25d1713c9f79773b
Alexander Wilkes <alexanderwilkes.personal@gmail.com>
2025-11-19 13:16:11 +0000

    tidy up

--- python/web_crawler/crawl.py
+++ python/web_crawler/crawl.py
@@ -4,13 +4,18 @@ from web_crawler.mt_webcrawler import MTWebCrawler
 from web_crawler.st_webcrawler import STWebCrawler
 from web_crawler.url import URL
 
+logger = logging.getLogger(__name__)
 
 def main(url: str, max_depth: int, multi_threaded: bool | None) -> None:
+    logging.basicConfig(level=logging.INFO)
+
     if multi_threaded:
-        logging.warning("Multi-threaded crawling will not terminate")
+        logger.info("Multi-threaded crawling")
+        logger.warning("Multi-threaded crawling will not terminate")
         wc = MTWebCrawler(max_depth=max_depth)
 
     else:
+        logger.info("Single-threaded crawling")
         wc = STWebCrawler(max_depth=max_depth)
 
     wc.crawl(URL(url))
@@ -26,7 +31,7 @@ if __name__ == "__main__":
         help="Starting URL to crawl",
         default="https://www.barrierreef.org/the-reef/animals/manta-ray",
     )
-    parser.add_argument("--depth", type=int, help="Maximum depth to crawl", default=2)
+    parser.add_argument("--depth", type=int, help="Maximum depth to crawl", default=1)
     parser.add_argument("--multi-threaded", action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
 
