67da10fbb02edacd7eeb20f0c365455a96d77450
ekta-kochar <ekta.kochar.1502@gmail.com>
2025-12-03 23:09:19 +0000

    Refactor Crawler class to enhance URL uniqueness handling in crawl method

--- python/site_crawler/crawler.py
+++ python/site_crawler/crawler.py
@@ -10,13 +10,15 @@ class Crawler:
     def __init__(self, base_url: str, concurrency: int = 10, timeout: int = 10, max_pages: int = 100, fetcher = Fetcher()) -> None:
         self._base_url = base_url
         self._fetcher = fetcher
+        self.seen_urls: Set[str] = set()
 
     async def crawl(self) -> AsyncGenerator[Set[str], None]:
         async with aiohttp.ClientSession() as session:
             content = await self._fetcher.fetch(session, url=self._base_url)
 
             urls = get_urls_from_html(content, self._base_url)
-            valid_urls = [url for url in urls if self.is_valid(url)]
+            unique_urls = [url for url in urls if url not in self.seen_urls]
+            valid_urls = [url for url in unique_urls if self.is_valid(url)]
 
             print(valid_urls)
 
