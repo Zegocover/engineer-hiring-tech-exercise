67b3e80b0feb405ff29dcb3e0d548fd4c26fc92c
e-larrambebere <emiliolarrambebere@gmail.com>
2026-07-12 17:00:03 +0100

    Initial setup
    Adds the basics of the project including:
    - uv setup and commands
    - initial set of dev dependencies
    - starting Python files for creating a cli tool

---
+++ crawler/src/cli.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+def main(argv: list[str] | None = None) -> int:
+    print("Test the CLI is invoked")
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
